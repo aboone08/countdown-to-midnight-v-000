9be3eed62b3d9365d6e448cf64e87a42e5d307cd
Andranique Boone <andranique.boone@gmail.com>
2019-01-24 02:27:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,14 @@
 #write your code here
 
-def countdown
-  x=0
-  while x = 10
-    puts "#{x} SECOND(S)!"
-    x -= 1
-    puts "HAPPY NEW YEAR!"
+def countdown(num)
+  while num>10
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    countdown_with_sleep(1)
+  end 
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  sleep num
 end
